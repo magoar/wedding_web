aaa3492e8adbc54e87a0be7faac4a0f417b9dc78
Mago Arcera <magocali44@gmail.com>
2025-10-27 13:06:57 +0100

    new edit button for guests and tables

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,7 +1,7 @@
 class GuestsController < ApplicationController
 
   def index
-    @guests = Guest.all
+    @guests = Guest.order(created_at: :desc)
     @total = @guests.sum { |g| 1 + (g.guests_count || 0) }
   end
   def new
@@ -17,6 +17,21 @@ class GuestsController < ApplicationController
     end
   end
 
+  def update
+    @guest = Guest.find(params[:id])
+    if @guest.update(guest_params)
+      respond_to do |format|
+        format.html { redirect_to guests_path, notice: "Guest updated successfully." }
+        format.json { render json: { guests_count: @guest.guests_count }, status: :ok }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to guests_path, alert: "Could not update guest." }
+        format.json { render json: { errors: @guest.errors.full_messages }, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def assign_table
     @guest = Guest.find(params[:id])
     if @guest.update(table_number: params[:table_number])
@@ -29,6 +44,17 @@ class GuestsController < ApplicationController
   private
 
   def guest_params
-    params.require(:guest).permit(:name, :guests_count, :message, :table_number)
+    gp = params.require(:guest).permit(:name, :message, :guests_count, :table_number)
+    if gp.key?(:table_number)
+      val = gp[:table_number]
+      if val.blank?
+        gp[:table_number] = nil
+      elsif val.to_s.strip.downcase == "vip"
+        gp[:table_number] = 22   # <-- map VIP to 22
+      else
+        gp[:table_number] = val.to_i
+      end
+    end
+    gp
   end
 end
